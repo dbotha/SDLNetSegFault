b944c0c42e8132d364b7561bda6864d216f5da86
Deon Botha <deon@dbotha.com>
2022-10-13 10:44:42 +0100

    Call SDLNet_ResolveHost on background thread

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
+#include <functional>
+#include <future>
 
 #include <SDL_net.h>
 
 int main() {
-    IPaddress ip;
-    if (SDLNet_ResolveHost(&ip, "www.google.com",
-                           443) == -1) {
-        std::cout << "Error: " << SDLNet_GetError() << std::endl;
-        return -1;
-    }
+    auto resolveFuture = std::async(std::launch::async, []() -> std::string{
+        IPaddress ip;
+        if (SDLNet_ResolveHost(&ip, "www.google.com",
+                               443) == -1) {
+            return SDLNet_GetError();
+        }
+        return "Success";
+    });
 
-    std::cout << "Success" << std::endl;
+    std::cout << resolveFuture.get() << std::endl;
     return 0;
 }
